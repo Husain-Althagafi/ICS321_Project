1816489968f962366186e435dd4c3a8d5eaa5b31
Husain-Althagafi <husain.a.althagafi@okaz.com>
2025-05-01 19:21:16 +0300

    catch up to main

---
+++ frontend/src/pages/Admin/AdminPage.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+
+function AdminPage() {
+  return (
+    <div className="admin-dashboard">
+      <h1>Admin Dashboard</h1>
+      {/* Future components will go here */}
+    </div>
+  );
+}
+
+export default AdminPage;
