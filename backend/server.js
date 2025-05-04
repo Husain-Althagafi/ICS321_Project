a3201d8b232f1d5d12072061ba3328e870ebdc9f
Husain-Althagafi <husain.a.althagafi@okaz.com>
2025-05-04 21:13:00 +0300

    uncommented some stuff

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,25 @@
 //Imports
 const express = require('express')
 const cors = require('cors')
+
+//Routes
 const authRoutes = require('./routes/authRoutes')
 const adminRoutes = require('./routes/adminRoutes')
-// const guestRoutes = require('./routes/guestRoutes')
+const guestRoutes = require('./routes/guestRoutes')
 
 
 //App
 const app = express()
 const PORT = 5000
-const DB_PORT = 5432
-
 
 //Middleware
-app.use(express.json())
 app.use(cors())
-
+app.use(express.json())
 
 //Routes
 app.use('/auth', authRoutes);
 app.use('/admin', adminRoutes);
-// app.use('/guest', guestRoutes);
+app.use('/guest', guestRoutes);
 
 
 //Server
